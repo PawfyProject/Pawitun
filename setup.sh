b5c16b9a0581d12de5007fc54478e3d540f20653
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-03-13 23:36:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
 sleep $((RANDOM % 20))
+
 yes | pkg install curl wget git -y
 yes | pkg update
 yes | pkg upgrade
+
 termux-setup-storage
+
 yes | pkg install lua53 sqlite termux-api -y
+
 wm size 1280x720
 wm density 192
+
 settings put global window_animation_scale 0
 settings put global transition_animation_scale 0
 settings put global animator_duration_scale 0
+
 cd /sdcard/Download
-curl -L -o winter-rejoin.lua https://api.wintercode.dev/loader/winter-rejoin.lua
-LICENSE="074399DD2D7361FCD398420BC6A0C3F7"
-while true
-do
-(
-sleep 10
-echo "$LICENSE"
-) | lua /sdcard/Download/winter-rejoin.lua </dev/null
-sleep 5
-done
+
+curl -L -o /sdcard/Download/winter-rejoin.lua https://api.wintercode.dev/loader/winter-rejoin.lua && lua /sdcard/Download/winter-rejoin.lua </dev/null
