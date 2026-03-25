779c1290e5001fbddf967ea4ed9e92acd62de6c0
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-03-26 01:38:04 +0700

    Update Panen.lua

--- Configs/Panen.lua
+++ Configs/Panen.lua
@@ -100,7 +100,7 @@ _G.FishItConfig = _G.FishItConfig or {
             "Reeler I",
             "Empowered I",
         },
-        ["Second Enchant"] = false,
+        ["Second Enchant"] = true,
         ["Allowed Sacrifice"] = {
             "Ghost Shark",
             "Gladiator Shark", 
