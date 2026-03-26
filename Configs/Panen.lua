18e78b81911783c84a5fff0857b8472141376683
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-03-26 23:50:23 +0700

    Update Panen.lua

--- Configs/Panen.lua
+++ Configs/Panen.lua
@@ -88,8 +88,8 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Start Farm"] = true,
         ["Minimum Rod"] = "Element Rod",
         ["Event List"] = {
-            "Thunderzilla Hunt",
 			["Ancient Lochness Monster"] = true,
+            "Thunderzilla Hunt",
             "Megalodon Hunt",
 		},
     },
