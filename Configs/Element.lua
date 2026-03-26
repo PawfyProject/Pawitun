ffa59cd228335bce003278b6835d0eae14ebeb12
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-03-26 22:33:48 +0700

    Update Element.lua

--- Configs/Element.lua
+++ Configs/Element.lua
@@ -43,7 +43,7 @@ _G.FishItConfig = _G.FishItConfig or {
     ["Doing Quest"] = {
         ["Auto Ghostfinn Rod"] = true,
         ["Auto Element Rod"] = false,
-		["Auto Element Rod 2"] = false,
+		["Auto Element Rod 2"] = true,
         ["Auto Diamond Rod"] = false,
         ["Unlock Ancient Ruin"] = true,
         ["Allowed Sacrifice"] = {
@@ -98,7 +98,7 @@ _G.FishItConfig = _G.FishItConfig or {
             "Reeler I",
             "Empowered I",
         },
-        ["Second Enchant"] = false,
+        ["Second Enchant"] = true,
         ["Allowed Sacrifice"] = {
             "Ghost Shark",
             "Gladiator Shark", 
