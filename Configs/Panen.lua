7e9622f42efbc3fe913756c84c8b08ba68e1d038
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-03-26 22:08:58 +0700

    Update Panen.lua

--- Configs/Panen.lua
+++ Configs/Panen.lua
@@ -18,7 +18,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Auto Trade"] = {
         ["Enabled"] = true,
-        ["Whitelist Username"] = {"lumibackup31"},
+        ["Whitelist Username"] = {"lumibackup32"},
         ["Category Fish"] = {
             "Secret",
             -- {Tier = "Mythic", Variant = "Stone"}, -- Tier + Variant
