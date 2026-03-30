01ad467fa52412dbf0fd0325bdf330e07da2ebbb
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-03-31 04:13:33 +0700

    Update auto_clear_crash.sh

--- auto_clear_crash.sh
+++ auto_clear_crash.sh
@@ -2,24 +2,18 @@
 
 TARGET="/storage/emulated/0/PunkX/crash.log"
 
+echo "Script started..."
+
 while true
 do
     if [ -f "$TARGET" ]; then
-        
-        SIZE=$(du -h "$TARGET" | cut -f1)
-        
-        echo "=============================="
-        echo "$(date)"
-        echo "File ditemukan!"
-        echo "Ukuran: $SIZE"
-        
+        SIZE=$(du -h "$TARGET" 2>/dev/null | cut -f1)
+
+        echo "$(date) | Found | Size: $SIZE"
         rm -f "$TARGET"
-        
-        echo "Status: BERHASIL DIHAPUS"
-        echo "=============================="
-        
+        echo "$(date) | Deleted"
     else
-        echo "$(date) - File tidak ditemukan"
+        echo "$(date) | Not found"
     fi
 
     sleep 14400
