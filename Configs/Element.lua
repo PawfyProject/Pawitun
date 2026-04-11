0bac2d54854ade1cedf512891c0a632e3cb2c654
Pawfy <34670335+dandansalsal@users.noreply.github.com>
2026-04-11 19:21:35 +0700

    Update Element.lua

--- Configs/Element.lua
+++ Configs/Element.lua
@@ -131,8 +131,8 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Location Rods"] = {
             ["Kohana Volcano"] = {"Starter Rod"},
             ["Tropical Grove"] = {"Grass Rod", "Midnight Rod"},
-            ["Treasure Room"] = {"Astral Rod","Diamond Rod","Element Rod"},	
-            ["Ancient Ruin"] = {"Ghostfinn Rod"},
+            ["Treasure Room"] = {"Astral Rod","Diamond Rod"},	
+            ["Ancient Ruin"] = {"Ghostfinn Rod","Element Rod"},
         },
         ["Endgame"] = "Diamond Rod",
     },
